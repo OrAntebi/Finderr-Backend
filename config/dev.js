bbb9b076e790fee4bf0fa8765e770d9ec68c9902
Farhan-Ganayim <f.ganayim@gmail.com>
2025-06-20 11:42:08 +0300

    Connect to data on atlas

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb+srv://farhan:farhan123@cluster0.jglrvhp.mongodb.net/',
   dbName : 'gig_db'
 }
